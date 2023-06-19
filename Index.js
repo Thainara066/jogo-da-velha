b039a0d8240b583e1cd61dd3849d5f5ad6261f7f
Thainara066 <127572393+Thainara066@users.noreply.github.com>
2023-06-19 19:37:37 -0300

    Clonando o Jogo da Velha do professor Rejman

---
+++ Index.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+
+function criarTabuleiro(){
+    const div = document.createElement("div");
+
+    const board = div.cloneNode(true);
+    board.classList.add("board");
+    body.appendChild(board);
+
+    const cell = div.cloneNode(true);
+    cell.classList.add("cell");
+
+    const quadrados = Array(9).fill(0);
+    quadrados.forEach((_,index) => {
+        let identificador = index +1;
+        cell.setAttribute("id", identificador);
+        cell.setAttribute("onclick", `marcarJogada(${identificador})`)
+        board.appendChild(cell.cloneNode(true));
+
+    });
+
+}
+
+criarTabuleiro();
